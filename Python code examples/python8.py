53decdced5e6cd1ff4038c3cf675a38a7c83ef49
forobor <demostapenko@gmail.com>
2017-02-04 03:13:06 +0400

    add Python examples

---
+++ Python code examples/python8.py
@@ -0,0 +1,28 @@
+﻿'''
+задачей будет восстановление исходной строки обратно.
+Напишите программу, которая считывает из файла строку, соответствующую тексту, 
+сжатому с помощью кодирования повторов, и производит обратную операцию, получая исходный текст.
+Sample Input:
+a3b4c2e10b1
+Sample Output:
+aaabbbbcceeeeeeeeeeb
+'''
+
+with open("dataset_3363_2.txt","r") as file:
+	s=file.readline()
+	s2=""
+	for i in range(len(s)):
+		#print(s[i])
+		if s[i].isdigit():
+			continue
+		else:
+			try:
+				if s[i+2].isdigit():
+					s2+=s[i]*int(s[i+1]+s[i+2])
+				else:
+					s2+=s[i]*int(s[i+1])
+			except IndexError:
+				break
+	print(s2)
+with open("ex.txt","w") as file:
+	file.write(s2)
---
+++ Python code examples/python9.py
@@ -0,0 +1,30 @@
+﻿'''
+Напишите программу, которая считывает текст из файла (в файле может быть больше одной строки)
+ и выводит самое частое слово в этом тексте и через пробел то, сколько раз оно встретилось. 
+ Если таких слов несколько, вывести лексикографически первое (можно использовать оператор < для строк).
+
+Слова, написанные в разных регистрах, считаются одинаковыми.
+
+Sample Input:
+abc a bCd bC AbC BC BCD bcd ABC
+Sample Output:
+abc 3
+'''
+
+with open("dataset_3363_3.txt",'r') as file:
+    s=file.read().lower().split()
+    print(s)
+    s2=""
+    c=0
+    for i in s:
+        if s.count(i)>c:
+            s2=i
+            c=s.count(i)
+        elif s.count(i)==c:
+            if s2> i:
+               s2=i 
+    print(s2,c)
+    
+with open("ex.txt",'w') as file:
+    file.write(s2 + " " + str(c))
+    
